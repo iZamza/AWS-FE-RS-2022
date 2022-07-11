5e20c51a931278a5eb38dfb45ad8a34329e8269f
Viktar Tsikhmanovich <Viktar_Tsikhmanovich@epam.com>
2022-07-11 17:08:04 +0200

    chnage pathes

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://jxr82rmw3a.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://1prix8ara7.execute-api.us-east-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
